8780aa0e34f5e7d18b317df637e736d67b381d98
Kurlok <felipe.augum@gmail.com>
2020-02-04 20:45:58 -0300

    Colocando erros em pt-br

---
+++ Votacao-Blockchain/resources/lang/pt-br/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas devem possuir ao menos 6 caracteres e serem iguais a sua confirmação.',
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Nós enviamos um e-mail para redefição de sua senha!',
+    'token' => 'Este token para redefinir a senha está inválido.',
+    'user' => "Não encontramos um usuário com este endereço de e-mail.",
+
+];
